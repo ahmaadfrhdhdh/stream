4e96de84a10a483b138ababa66178b218f207376
ahmaadfrhdhdh <45124321+ahmaadfrhdhdh@users.noreply.github.com>
2018-11-26 15:32:30 +0300

    Update Code.js

--- Code.js
+++ Code.js
@@ -1,12 +1,12 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-const prefix = "#"
+const prefix = "Mst"
 client.on('message', message => {
   if (!message.content.startsWith(Ast)) return;
   var args = message.content.split(' ').slice(1);
   var argresult = args.join(' ');
-  if (message.author.id !== '') return;
+  if (message.author.id !== 'NDExODY2MjI0NjkyMDM1NTg0.Dt12Hg.8DR7aTFCdj65F_qhP0tIPlXA8lY') return;
 
 if (message.content.startsWith(prefix + 's')) {
   client.user.setGame(argresult, "https://www.twitch.tv/Justin-Ly0001%22);
@@ -15,4 +15,4 @@ if (message.content.startsWith(prefix + 's')) {
 
 });
 
-client.login(NDExODY2MjI0NjkyMDM1NTg0.Dt12Hg.8DR7aTFCdj65F_qhP0tIPlXA8lY);
+client.login(TOKEN);
